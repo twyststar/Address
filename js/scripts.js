f1c2321ef7a4977e664c0fe1eb7dab2542213572
Starlynne <lewis.start0905@gmail.com>
2017-04-03 20:33:42 -0700

    Add constructor for addresses, add property for addresses to contact contsructor

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,13 @@
 function Contact(first, last) {
   this.firstName = first;
   this.lastName = last;
+  this.addresses = [];
+}
+function Address(street, city, state, zip) {
+  this.street = street;
+  this.city = city;
+  this.state = state;
+  this.zip = zip;
 }
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
